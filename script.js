54b487bfa3c1b0964e781fce852db3d39625646f
Kevin Liu <you@example.com>
2023-06-14 14:21:53 -0400

    Updated Form

--- script.js
+++
@@ -1,5 +0,0 @@
-// In a Javascript file, write a program which checks the following things:
-// checks that the password is 12345678
-// if the password is incorrect, send an alert message
-// Your page should also include an <h1> tag. 
-// If the information in the form is correct, have Javascript change the text in the <h1> .HTML page.
